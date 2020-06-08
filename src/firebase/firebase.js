24a81d2cfd976e3a23ff11f47f0f77649bcd0f4e
dvd2x <black.god1930@gmail.com>
2020-06-08 11:54:32 -0400

    up dated firbase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,10 +1,9 @@
-import * as firebase from "firebase";
-import "firebase/auth";
-import "firebase/database";
+import * as firebase from 'firebase';
+
 // Your web app's Firebase configuration
 const config = {
   apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAN,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.FIREBASE_DATABASE_URL,
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
